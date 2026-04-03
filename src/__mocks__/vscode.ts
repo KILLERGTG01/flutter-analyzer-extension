b2c82a662e79b6108f04edad46d87a45cf3e4b4c
KILLERGTG01 <gtg07817@gmail.com>
2026-04-03 13:10:26 +0530

    chore: project scaffold with jest config and vscode mock

---
+++ src/__mocks__/vscode.ts
@@ -0,0 +1,11 @@
+export const workspace = {
+  getConfiguration: () => ({
+    get: <T>(_key: string, defaultValue: T): T => defaultValue,
+  }),
+};
+export const DiagnosticSeverity = { Warning: 1 };
+export const languages = {};
+export const window = {};
+export const env = {};
+export const commands = {};
+export const CodeActionKind = { QuickFix: { value: 'quickfix' } };
